cde75d9eb31d6e44908c9e333fd03a9be160c2c8
Ana Carla <anacarlarosa@alunos.utfpr.edu.br>
2025-12-02 17:20:09 -0300

    feat: add toast message

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,11 +4,14 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
 import { AuthProvider } from './contexts/AuthContext';
 import './styles/global.css';
+import { ToastProvider } from './contexts/ToastContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ToastProvider>
+        <RouterProvider router={router} />
+      </ToastProvider>
     </AuthProvider>
   </React.StrictMode>
 );
